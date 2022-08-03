f7db34286418f7a6963dffa5513ea85e55976d0a
Ranjana Yadav <yranjana757@gmail.com>
2022-08-03 21:11:58 +0530

    image section .

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export const About = ({ mode }) => {
         <div className='container my-5' style={{ color: mode === "dark" ? "white" : "black" }}>
             <h1>About Text-Utilis:🚀 </h1>
             <p>TextUtils is a word counter and a character counting utility which can be used to manipulate your text in the way you want.</p>
-            <img src="../../Text-Utilis-about.png" className='my-4 mb-5' alt="Text-Utilis" />
+            <img src="../../Text-Utilis-about.png" className='my-4 mb-5 container' alt="Text-Utilis" />
             <h2 className='my-4'>Feature of Using Text-Utilis :</h2>
             <h5 className='mb-4'>By using text Utilis you can perform following operaion on the text ..</h5>
             <ul>
