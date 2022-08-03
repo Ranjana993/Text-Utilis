cb0dfa64882ca389c0a730ba34aced60d6a7b2ae
Ranjana Yadav <yranjana757@gmail.com>
2022-08-03 20:40:01 +0530

    removed collapse from navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = ({ mode, toggleMode }) => {
         <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}  `}>
             <div className="container-fluid ">
                 <Link className="navbar-brand " to="/">Text-Utilis</Link>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
